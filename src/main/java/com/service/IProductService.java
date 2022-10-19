cbd2cea118694c58f6d2d5d09669542069a3fb84
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-10-19 09:28:36 +0700

    19/10

---
+++ src/main/java/com/service/IProductService.java
@@ -0,0 +1,18 @@
+package com.service;
+
+import com.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    List<Product> findAll();
+
+    void save(Product product);
+
+    Product findById(int id);
+
+    void update(int id, Product product);
+
+    void remove(int id);
+
+}
